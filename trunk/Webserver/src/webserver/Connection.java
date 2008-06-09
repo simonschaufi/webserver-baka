061ab6299296b33bb0f7d6028c87acd78daf87ca
JoergMogielinski <JoergMogielinski@3741392c-a84d-0410-a02f-a7191b7d6815>
2008-06-09 18:57:25 +0000

    Alle 3 Zusammen

--- trunk/Webserver/src/webserver/Connection.java
+++ trunk/Webserver/src/webserver/Connection.java
@@ -1,12 +1,7 @@
+
 /*
  * Connection.java
- *
- * Created on 19. Juni 2007, 10:41
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
  */
-
 package webserver;
 
 import java.io.BufferedReader;
@@ -15,109 +10,194 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+
 /**
  *
- * @author schwendemann.sebasti
+ * @author Andreas Paul
  */
-public class Connection extends Thread{
-    
-    /** Creates a new instance of Connection */
-    Socket theSocket;
-    BufferedReader input;
-    OutputStream out;
-    Message theMessage;
-    DataInputStream dataInput;
-    ClientRequest request;
-    Interface gui;
-    
-    
-    public Connection(Socket aSocket, Interface i) 
-    {
-        this.gui = i;
-        try
-        {   
-            theSocket = aSocket;
-            theSocket.setKeepAlive(true);
-            input = new BufferedReader(new InputStreamReader(aSocket.getInputStream()));
-            out = theSocket.getOutputStream();
-        }
-        catch(IOException e)
-        {
-            gui.printMessages("Fehler beim erstellen der Connection-Klasse: "+e.getMessage());
-        }
-    }
-    
-    public void run()
-    {
-        try{
-            while(!(theSocket.isClosed())){
-                if(theSocket.getInputStream().available()!=0){
-                    sleep(50);
-                    read();
-                    if(!(request.getKeepAlive().equalsIgnoreCase("Keep-Alive")) || request.getHttpVersion().equalsIgnoreCase("HTTP/1.0"))
-                        theSocket.close();
-                    gui.printMessages("Socket: "+theSocket.getPort());
-                    gui.printMessages(" ");
-                }
-                sleep(10);
-            }    
-        }catch(IOException e){
-            gui.printMessages("Fehler in der persistenten Verbindung: "+ e.getMessage());
-        }catch(InterruptedException e)
-        {   
-            gui.printMessages("Fehler, Socket wurde geschlossen: "+e.getMessage());
+public class Connection extends Thread {
+
+    Socket sSocket; //Deklarieren der Socket-Variable
+    BufferedReader brInput; //Deklarieren der BufferedReader-Variable
+    OutputStream osOutput; //Deklarieren der OutputStream-Variable
+    Message mMessage; //Deklarieren der Message-Variable
+    DataInputStream dataInput; //Deklarieren der DataInputStream-Variable
+    Request cRequest; //Deklarieren der ClientRequest-Variable
+    Interface intfGui; //Deklarieren der Interface-Variable
+
+    public Connection(Socket aSocket, Interface intf) {
+        this.intfGui = intf;
+        /*^^^^
+         * Durch Aufruf dieses Konstruktors durch die Klasse 'Server' in ihrer 'run'-Methode
+         * wird der zu verwendende Socket und das genutzte Objekt der Klasse 'Interface'
+         * für ihre 'printMessages'-Methode mitgegeben
+         */
+
+        try {
+            sSocket = aSocket; // Der durch den Konstruktoraufruf mitgegebene Socket wird zum Socket dieses 'Connection'-Objektes
+            sSocket.setKeepAlive(true); //Lässt den Socket offen
+            brInput = new BufferedReader(new InputStreamReader(aSocket.getInputStream())); //Der Inputstream des Sockets wird in der BufferedReader-Variable 'brInput' gespeichert
+            osOutput = sSocket.getOutputStream(); //Der Outputstream des Sockets wird in der OutputStream-Variable 'osOutput' gespeichert
+        } catch (IOException ieEx) { //Abfangen eines möglichen Fehler beim Erstellen des 'Connection'-Objektes
+            intfGui.printMessages("Beim Erstellen eines Connection-Objektes ist ein Fehler aufgetretten: " + ieEx.getMessage() + "!");
         }
     }
-    
-    private void read()
-    {        
-        try
-        {                    
-            request = new ClientRequest(theSocket.getInputStream(), gui);
-            theMessage = new Message(request, out, gui);
-            String kindOfMessage = request.getKindOfRequest();
-            if(kindOfMessage.equals("GET"))
-            {
-                if(Settings.use100Continue)
-                {
-                   theMessage.send100Continue();
-                   try
-                   {
-                     sleep(5000);
-                   }
-                   catch(InterruptedException e)
-                   {
-                       gui.printMessages("100 continue - unable to sleep: "+e.getMessage());
-                   }
+
+    @Override
+    public void run() { //Die 'run'-Methode der 'Connection'-Klasse
+        try {
+            while (!(sSocket.isClosed())) { //Solange der Socket nicht geschlossen ist!
+                if (sSocket.getInputStream().available() != 0) {
+
+                    /*^^^^
+                     * 'sSocket.getInputStream().available()' gibt die zu erwartende Bytes-Anzahl des Inputstream des Sockets wieder.
+                     * Wenn diese nicht '0' beträgt führe die klasseninterne Methode 'read' aus.
+                     */
+
+                    sleep(100); // Warte bis der Browser genügend Zeit hatte Daten an den Socket zu schicken
+                    read(); //Lese
+                    if (!(cRequest.getPersistentConnection().equalsIgnoreCase("Keep-Alive")) || cRequest.getVersion().equalsIgnoreCase("HTTP/1.0")) {
+                        sSocket.close();
+                    }
+                    intfGui.printMessages("Socket: " + sSocket.getPort());
+                    intfGui.printMessages(" ");
                 }
-                theMessage.replyRequest(true);
-            }
-            else if(kindOfMessage.equals("POST"))
-            {              
-                theMessage.writePostToFile(request.getPostContent()); 
-                new CreateResponse(out, request, "204 No Content", gui).generateResponse("STATUS");
-            }
-            else if(kindOfMessage.equals("HEAD"))
-            {
-                theMessage.replyRequest(false);
-            }
-            else if(kindOfMessage.equals("PUT") || kindOfMessage.equals("DELETE") || kindOfMessage.equals("OPTIONS")
-                    || kindOfMessage.equals("TRACE")){
-                throw new ClientException("501 Not Implemented");
-            }       
-            else
-                throw new ClientException("400 Bad Request");
-                
+                sleep(50);
+
+            }  //#### Hier endet die while (!(sSocket.isClosed()))-Schleife ####
+
+        } catch (IOException ioEx) {
+            intfGui.printMessages("Fehler in der Verbindung: " + ioEx.getMessage() + "!");
+        } catch (InterruptedException ieEx) {
+            intfGui.printMessages("Socket wurde aufgrund eines Fehlers geschlossen: " + ieEx.getMessage() + "!");
         }
-        catch(IOException e)
-        {
-            gui.printMessages("Fehler beim Ermitteln des Client-Requests: "+e.getMessage()); 
+    }
+
+    private void read() {
+        try {
+            cRequest = new Request(sSocket.getInputStream(), intfGui);
+            /*^^^^
+             * Erstelle ein neues Objekt der Klasse 'ClientRequest'
+             * Gebe diesem Objekt den momentanen Inputstream des Sockets mit
+             * und das verwendete GUI für die 'GUI'-Methode 'printMessages'
+             */  
+
+            mMessage = new Message(cRequest, osOutput, intfGui);
+            /*^^^^
+             * Erstelle ein neues Objekt der Klasse 'Message'
+             * Gebe diesem Objekt den momentanen Outputstream des Sockets mit,
+             * das zuvor erstellte 'ClientRequest'-Objekt 'cRequest'
+             * und das verwendete GUI für die 'GUI'-Methode 'printMessages'
+             */
+
+            String kOfMsgReq = cRequest.getKindOfRequest();
+            /*^^^^
+             * Holt sich die Art des Clientrequestes durch die 'ClientRequest'-Methode 'getKindOfRequest'
+             * und speichert es als lokale String-Variable
+             * 
+             * In 'kOfMsgReq' kann: GET, POST, HEAD, PUT, TRACE, ,DELTE, OPTIONS stehen
+             */
+
+            if (kOfMsgReq.equals("GET")) { // Wenn die Art des Clientrequestes ein 'GET' ist
+                if (Settings.use100Continue) {
+                    /*^^^^
+                     * In den Einstellungen des Webserver ist die Verwendung von 100 HTTP-Statuscodes erlaubt
+                     * Die laufende Anfrage an den Server wurde noch nicht zurückgewiesen.
+                     * Der Client kann nun mit der (potentiell sehr großen) Anfrage fortfahren.
+                     */
+
+                    mMessage.send100Continue();
+                    /*^^^^
+                     * Die Methode 'send100Continue' der 'Message' wird aufgerufen
+                     * Diese sendet folgendes an den Outputstream: 
+                     * String con = "HTTP/1.1 100 Continue" + ("\r\n") + ("\r\n");
+                     */
+
+                    try {
+                        sleep(5000); //Der Server-Thread versucht 5 Sekunden zu schlafen
+                    } catch (InterruptedException ieEx) { //Abfangen, wenn der Server-Thread durch ein 'send100Continue' unterbrochen wird
+                        intfGui.printMessages("unable to sleep -> 100 continue : " + ieEx.getMessage() + "!");
+                    }
+
+                } //#### Hier endet Settings.use100Continue-IF-Abfrage ####
+
+
+                mMessage.replyRequest(true);
+
+            /* ^^^^
+             * Ruft die Methode 'replyRequest' der Klasse 'Message' auf
+             * 
+             * Diese überprüft, ob die vom Clienten angeforderte Datei:
+             * a) existiert, b) gelesen werden kann und c) kein Ordner ist.
+             *
+             * Ausserdem wird durch den überprüft, ob die Datei nach der letzten Anforderung geändert wurde
+             * Wenn ja wird ein HTTP-Status-Code '304' versendet
+             * --> "Indicates the resource has not been modified since last requested."
+             * 
+             * Zum Schluss wird die angeforderte Datei gesendet.
+             */
+
+            } else if (kOfMsgReq.equals("POST")) {  // Wenn die Art des Clientrequestes ein 'POST' ist
+                mMessage.writePostToFile(cRequest.getPostKeyValueString());
+                /* ^^^^
+                 * Hier wird der vom Clienten versendete 'POST'-Inhalt in einer Datei gespeichert
+                 */
+
+                new CreateResponse(osOutput, cRequest, "204  -> No Content", intfGui).generateResponse("STATUS");
+            /*^^^^
+             * Die Anfrage wurde erfolgreich durchgeführt, die Antwort enthält jedoch bewusst keine Daten.
+             */
+
+            } else if (kOfMsgReq.equals("HEAD")) {  // Wenn die Art des Clientrequestes ein 'HEAD' ist
+                mMessage.replyRequest(false);
+            /* ^^^^
+             * Ruft die Methode 'replyRequest' der Klasse 'Message' auf
+             * 
+             * Diese überprüft, ob die vom Clienten angeforderte Datei:
+             * a) existiert, b) gelesen werden kann und c) kein Ordner ist.
+             *
+             * Ausserdem wird durch den überprüft, ob die Datei nach der letzten Anforderung geändert wurde
+             * Wenn ja wird ein HTTP-Status-Code '304' versendet.
+             * --> "Indicates the resource has not been modified since last requested."
+             * 
+             * Die eigntliche Datei wird NICHT (!) gesendet,
+             * sondern nur 'content-type' und 'content-length'.
+             */
+
+            } else if (kOfMsgReq.equals("PUT") || kOfMsgReq.equals("DELETE") || kOfMsgReq.equals("OPTIONS") || kOfMsgReq.equals("TRACE")) {
+                // Wenn die Art des Clientrequestes etwas anderes als die zuvor abgeprüften ist
+                throw new ClientException("501  -> Not Implemented");
+            /*^^^^
+             * Da diese Funktionen nicht implementiert sind wird der entsprechende HTTP-Status-Code '501' gesendet
+             * 
+             * "Die Funktionalität, um die Anfrage zu bearbeiten, wird von diesem Server nicht bereitgestellt. 
+             * Ursache ist zum Beispiel eine unbekannte oder nicht unterstützte HTTP-Methode."
+             * 
+             */
+
+            } else { // Wenn in der Variable 'kOfMsgReq' kein bekannter (hier abgeprüfter) String vorhanden ist 
+                throw new ClientException("400  -> Bad Request");
+            }
+
+        } //#### Hier endet das try der 'read'-Methode #### catch (IOException e) {
+        catch (IOException ioEx) {
+            //Abfangen von fehlerhaften Interpretation des Client-Requests
+            intfGui.printMessages("Beim Ermitteln des Client-Requests ist ein Fehler aufgetretten: " + ioEx.getMessage());
+
+        } catch (ClientException clEx) {
+            //Gibt den Fehler aus, wenn 'kOfMsgReq' nicht POST, GET oder HEAD war
+            intfGui.printMessages("Fehler: " + clEx.getMessage());
+
+            CreateResponse cResponse = new CreateResponse(osOutput, clEx.getMessage(), cRequest, intfGui);
+
+            cResponse.setPath(cRequest.getRequestedFilePath());
+            /* ^^^^
+             * Ändert die 'path' Variable des Objektes 'cResponse' zu der 'path' Variable des Objektes 'cRequest'
+             */
+
+            cResponse.generateResponse("ERROR");
         }
-        catch(ClientException e){
-            gui.printMessages("Fehler: "+e.getMessage());
-            CreateResponse rhes = new CreateResponse(out,e.getMessage(),request, gui);
-            rhes.setPath(request.getPath());
-            rhes.generateResponse("ERROR");
-        }    
-    }    
+    }
 }
+
+    
---
+++ trunk/Webserver/src/webserver/Request.java
@@ -0,0 +1,222 @@
+
+package webserver;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+
+/**
+* Emppängt den Request vom Clienet(Browser) und "parst" den Header
+ * und settz dementsprechend Attribute der Klasse
+ */
+public class Request
+{
+
+    DataInputStream inputStream;
+    byte[] InputStreamBytes;
+    ArrayList<String> receivedHeader;
+    Interface gui;
+    String version = "";
+    String headerType = "";
+    String requestedFilePath = null;
+    String requestedHost = null;
+    String persistentConnection = "";
+    String postKeyValueString = "";
+    String modifiedSince = "";
+    String unmodifiedSince = "";
+
+    /**
+     * * 
+     * Konstruktor der den Input gleich zerlegt
+     * @param in der InputStream des Sockets
+     */
+    public Request(InputStream inputStream, Interface i)
+    {
+        this.gui = i;
+        this.inputStream = new DataInputStream(inputStream);
+        this.receivedHeader = new ArrayList<String>();
+        inputStreamToByteArray();
+        byteArrayToArrayList();
+        getRequestData();
+        checkModifiedUnmodifiedSince();
+    }
+
+    /**
+     * Jede Zeile des Requests wird als aneienandrgehängte chars in ein Feld der
+     * ArrayList geschrieben
+     */
+    private void byteArrayToArrayList()
+    {
+        final int carriageReturn = 13;
+        final int lineFeed = 10;
+        int length = InputStreamBytes.length;
+        String line = "";
+        for (int i = 0; i < length; i++)
+        {
+            if ((i < length - 2) && (InputStreamBytes[i] != carriageReturn && InputStreamBytes[i + 1] != lineFeed))
+                line = line + (char)InputStreamBytes[i];
+            else
+            {//vorletzte Zeile, oder ende der Zeile hier springt er immer rein
+                line = line + (char) InputStreamBytes[i] + (char) InputStreamBytes[i + 1];
+                receivedHeader.add(line);
+                line = "";
+                i++;
+            }
+        }
+    }
+
+    /**
+     * Schreibt den Kompletten Request in ein ByteArray.
+     */
+    private void inputStreamToByteArray()
+    {
+        try
+        {
+            int numberOfBytes = inputStream.available();
+            InputStreamBytes = new byte[numberOfBytes];
+            inputStream.readFully(InputStreamBytes);
+        }
+        catch (IOException e)
+        {
+            gui.printMessages("Fehler beim lesen des InputStreams!");
+        }
+    }
+
+    /**
+     * Zerteilt den Imput in Tokens (mittels String-Tokenizer) und Prüft auf 
+     * HTTP-Header inhalte
+     */
+    private void getRequestData()
+    {
+        String nextToken = "";
+        String lineBefore = "";
+        for (String singleLine : receivedHeader)
+        {
+            StringTokenizer stringToken = new StringTokenizer(singleLine, " ");
+            nextToken = stringToken.nextToken();
+            if (nextToken.equals("GET") || nextToken.equals("POST") || nextToken.equals("HEAD") || nextToken.equals("PUT") || nextToken.equals("DELETE") || nextToken.equals("OPTIONS") || nextToken.equals("TRACE"))
+            {
+                headerType = nextToken;
+                requestedFilePath = stringToken.nextToken();
+                version = stringToken.nextToken().replaceAll("\r", "").replaceAll("\n", "");
+            }
+            else if (nextToken.equals("Host:"))
+            {
+                requestedHost = (stringToken.nextToken()).trim();
+                if(requestedHost.contains(":"))
+                requestedHost = requestedHost.substring(0, requestedHost.indexOf(":"));
+
+            }
+            else if (lineBefore.equals("/r/n"))
+            {
+                postKeyValueString = singleLine;
+            }
+            lineBefore = singleLine;
+        }
+        if (version.equals(""))
+            version = "HTTP/1.0";
+    }
+
+    /**
+     * Findet IfModiefiedSince und IfUnmidiefiertSince Informationen
+     * und speichert dies als Attribute ab
+     */
+    private void checkModifiedUnmodifiedSince()
+    {
+        for (String singleLine : receivedHeader)
+        {
+            //Position des Eintrages Filtern
+            int indexOfState = singleLine.indexOf("If-Modified-Since:");
+            //Falls in dieser Zeile nicht gesetzt
+            if (indexOfState == -1)
+                continue;
+            //Index des State + 19 Zeichen weiter steht das GMT-Datum
+            modifiedSince = singleLine.substring(indexOfState + 19);
+        }
+
+        for (String singleLine : receivedHeader)
+        {
+            int indexOfState = singleLine.indexOf("If-Unmodified-Since:");
+            if (indexOfState == -1)
+                continue;
+            unmodifiedSince = singleLine.substring(indexOfState + 21);
+        }
+    }
+
+    /** Pfad der Requesteten Datei aus der URL filtern,
+     * Anhand des Hostnamens den Entsprechenden Root-Pfad davor setzen
+     * @return Pfad aus Root-Pfad\\Dateipfad
+     */
+    public String getFilePath() throws ClientException
+    {
+        String completeFilePath;
+        if (requestedHost == null)
+        {
+            if (requestedFilePath.startsWith("http:"))
+            {
+                StringTokenizer stringTokens = new StringTokenizer(requestedFilePath, "/");
+                stringTokens.nextToken();
+                requestedHost = stringTokens.nextToken();
+                requestedFilePath = requestedFilePath.replaceAll("http://" + requestedHost, "");
+            }
+            else
+                throw new ClientException("400");
+        }
+        String rootOfHost = Settings.getRoot(requestedHost);
+        if (rootOfHost == null)
+            throw new ClientException("400");
+        completeFilePath = (rootOfHost + requestedFilePath);
+        completeFilePath = completeFilePath.replaceAll("/", Matcher.quoteReplacement("\\"));
+        return completeFilePath;
+    }
+   
+    /**
+     * getter-Methoden
+     */
+    
+    public String getVersion()
+    {
+        return version;
+    }
+
+    public String getKindOfRequest()
+    {
+        return headerType;
+    }
+
+    public String getRequestedFilePath()
+    {
+        return requestedFilePath;
+    }
+    
+    public String getRequestedHost()
+    {
+        return requestedHost;
+    }
+
+    /**
+     * aka keep alive
+     */
+    public String getPersistentConnection()
+    {
+        return persistentConnection;
+    }
+
+    public String getPostKeyValueString()
+    {
+        return postKeyValueString;
+    }
+
+    public String getModifiedSince()
+    {
+        return modifiedSince;
+    }
+
+    public String getUnModifiedSince()
+    {
+        return unmodifiedSince;
+    }
+}
